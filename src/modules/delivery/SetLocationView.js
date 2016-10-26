2c8bff076544147f23f58958b555bcbeeb44e266
Javier Tarazaga <javier.tarazaga@valtech.se>
2016-10-26 23:15:49 +0200

    Small code styling

--- src/modules/delivery/SetLocationView.js
+++ src/modules/delivery/SetLocationView.js
@@ -11,6 +11,7 @@ import {View, Content} from "native-base";
 import DefaultNavBar from "../../components/DefaultNavBar";
 import {GooglePlacesAutocomplete} from "react-native-google-places-autocomplete";
 
+// TODO - Make sure this is not hardcoded but setup by the user
 const homePlace = {description: 'Home', geometry: {location: {lat: 48.8152937, lng: 2.4597668}}};
 const workPlace = {description: 'Work', geometry: {location: {lat: 48.8496818, lng: 2.2940881}}};
 
@@ -50,31 +51,28 @@ class SetLocationView extends Component {
             fetchDetails={true}
             onPress={(data, details = null) => this.handlePlacePress(data, details)}
             getDefaultValue={() => {
-          return ''; // text input default value
-        }}
+              return ''; // text input default value
+            }}
             query={{
-          // available options: https://developers.google.com/places/web-service/autocomplete
-          key: 'AIzaSyBodeCxWCFMML6JvWL8MW6ztpHJZBN8KTw',
-          language: 'en' // language of the results
-        }}
+              // available options: https://developers.google.com/places/web-service/autocomplete
+              key: 'AIzaSyBodeCxWCFMML6JvWL8MW6ztpHJZBN8KTw',
+              language: 'en' // language of the results
+            }}
             styles={{
-          description: {
-            fontWeight: 'bold'
-          },
-          predefinedPlacesDescription: {
-            color: '#1faadb'
-          }
-        }}
+              description: {
+                fontWeight: 'bold'
+              },
+              predefinedPlacesDescription: {
+                color: '#1faadb'
+              }
+            }}
             currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
             currentLocationLabel="Current location"
             nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-            GoogleReverseGeocodingQuery={{
-          // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-        }}
             GooglePlacesSearchQuery={{
-          // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-          rankby: 'distance'
-        }}
+              // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
+              rankby: 'distance'
+            }}
             filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
 
             predefinedPlaces={[homePlace, workPlace]}
