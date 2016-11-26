6089f5aa956cc3c1234b3ff12de76efba55b4c6b
Javier Tarazaga <javier.tarazaga@valtech.se>
2016-11-26 10:19:16 +0100

    Added comment for getProfile epic

--- src/reducers/user/profile/epics/getProfile.js
+++ src/reducers/user/profile/epics/getProfile.js
@@ -1,3 +1,5 @@
+'use strict'
+
 import BackendFactory from "../../../../lib/BackendFactory"
 import AppAuthToken from "../../../../lib/__mocks__/AppAuthToken"
 
@@ -12,6 +14,14 @@ import "rxjs/add/operator/switchMap"
 import "rxjs/add/operator/map"
 import "rxjs/add/operator/catch"
 
+/**
+ * Epic fetching the user profile when the action {@link ActionTypes.GET_PROFILE_REQUEST} gets fired. The epic will
+ *
+ * In order to do so, this epic will fetch the auth sessionToken previously in order to communicate with the server.
+ *
+ * @param action$
+ * @return {Observable<R|I>}
+ */
 export default function getProfile(action$) {
   return action$.ofType(ActionTypes.GET_PROFILE_REQUEST)
     .switchMap(() =>
@@ -25,4 +35,3 @@ export default function getProfile(action$) {
         ))
     )
 }
-
