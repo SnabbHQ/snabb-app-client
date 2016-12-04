34f3894bcc23419e22ad53dcaf32e20b8e352b24
Javier Tarazaga <javier.tarazaga@valtech.se>
2016-12-05 00:07:06 +0100

    Added a bunch of new code

--- src/browser/home/HomePage.js
+++ src/browser/home/HomePage.js
@@ -1,37 +1,24 @@
 /* @flow */
 import React from "react"
-import {Block, Panel, PanelHeader, PanelFooter, Image, Link, PageHeader, SwitchTheme, Title, View} from "../app/components"
+import {Space, Block, Panel, PanelHeader, PanelFooter, Image, Link, PageHeader, SwitchTheme, Title, View} from "../app/components"
 import { Flex, Box } from 'reflexbox'
+import { Card, Form } from 'antd'
 import Map from './Map';
 
 const HomePage = () => {
 
   return (
     <Flex p={2} align='center'>
-      <Box col={4}>
-        <View>
-          <Title message="Snabb. Delivering Now"/>
-          <PageHeader
-            heading="Snabb"
-            description="One stack for browser, mobile, server."
-          />
-          {/* This is a block with margin-bottom: scale[4]. Inline styles rocks. */}
-          <Block mb={4}>
-            <Link to="https://github.com/este/este">
-              github.com/este/este
-            </Link>
-          </Block>
-          <SwitchTheme />
-          <Image
-            alt="50x50 placeholder"
-            mt={2}
-            src={require('./50x50.png')}
-          />
-        </View>
+      <Box col={5}>
+        <Card title="Request Pickup" style={{width: '100%'}}>
+          <Form></Form>
+        </Card>
+        <Space x={2} />
+        <Card title="Request Pickup">
+          <Form></Form>
+        </Card>
       </Box>
       <Box col={8} flexAuto>
-        <View style={{backgroundColor: 'black'}}/>
-        <Map/>
       </Box>
     </Flex>
   )
---
+++ src/common/lib/getPosition.js
@@ -0,0 +1,47 @@
+import request from 'superagent';
+
+const FREEGEOIP_URL = 'https://freegeoip.net/json/';
+
+function getPositionFromIp() {
+  return new Promise((resolve, reject) => {
+    request.get(FREEGEOIP_URL).end((error, response) => {
+      if (error) { return reject(error); }
+
+      const { longitude, latitude } = response.body;
+
+      resolve({
+        longitude,
+        latitude
+      });
+    });
+  });
+}
+
+const n = global.navigator;
+const hasGeolocation = n && n.geolocation && n.geolocation.getCurrentPosition;
+
+const GEOLOCATION_OPTIONS = {
+  maximumAge: 20 * 60 * 1000
+};
+
+export default function getPosition() {
+  if (!hasGeolocation) { return getPositionFromIp(); }
+
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((r) => {
+      const { longitude, latitude } = r.coords;
+
+      resolve({
+        longitude,
+        latitude
+      });
+    }, () => {
+      return resolve(getPositionFromIp());
+    }, GEOLOCATION_OPTIONS);
+  });
+}
+
+
+
+// WEBPACK FOOTER //
+// ./src/lib/getPosition.js
