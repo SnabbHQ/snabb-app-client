36a3c886c3bea4fdd57fbbdf7dedd550c7d176e5
Javier Tarazaga <javier.tarazaga@valtech.se>
2016-12-20 09:50:27 +0100

    Added forgot password link to login page

--- src/browser/auth/LoginFields.js
+++ src/browser/auth/LoginFields.js
@@ -2,12 +2,12 @@
 import type { State } from '../../common/types';
 import React from 'react';
 import buttonsMessages from '../../common/app/buttonsMessages';
-import emailMessages from '../../common/auth/authMessages';
+import authMessages from '../../common/auth/authMessages';
 import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 import { connect } from 'react-redux';
 import { fields } from '../../common/lib/redux-fields';
 import { login } from '../../common/auth/actions';
-import { Button, Input, Box, Card } from '../app/components';
+import { Button, Input, Link, Box } from '../app/components';
 import { Form, focus } from '../app/components-old';
 
 class LoginFields extends React.Component {
@@ -25,34 +25,46 @@ class LoginFields extends React.Component {
     const { disabled, fields, intl } = this.props;
 
     return (
-      <Form onSubmit={this.onFormSubmit} small>
-        <Card width="400px">
-          <Input
-            {...fields.email}
-            disabled={disabled}
-            label={intl.formatMessage(emailMessages.emailLabel)}
-            labelSize={-1}
-            maxLength={100}
-            padding="0.5em"
-            placeholder={intl.formatMessage(emailMessages.emailPlaceholder)}
-          />
-          <Input
-            {...fields.password}
-            disabled={disabled}
-            label={intl.formatMessage(emailMessages.passwordLabel)}
-            labelSize={-1}
-            maxLength={1000}
-            padding="0.5em"
-            placeholder={intl.formatMessage(emailMessages.passwordPlaceholder)}
-            type="password"
-          />
-          <Box marginTop="1em">
-            <Button width="100%" disabled={disabled} align="center">
-              <FormattedMessage {...buttonsMessages.signIn} />
-            </Button>
+      <Box width="350px">
+        <Form onSubmit={this.onFormSubmit} small>
+          <Box>
+            <Input
+              {...fields.email}
+              disabled={disabled}
+              label={intl.formatMessage(authMessages.emailLabel)}
+              labelSize={-1}
+              maxLength={100}
+              padding="0.5em"
+              placeholder={intl.formatMessage(authMessages.emailPlaceholder)}
+            />
+            <Input
+              {...fields.password}
+              disabled={disabled}
+              label={intl.formatMessage(authMessages.passwordLabel)}
+              labelSize={-1}
+              maxLength={1000}
+              padding="0.5em"
+              placeholder={intl.formatMessage(authMessages.passwordPlaceholder)}
+              type="password"
+            />
+            <Box marginTop="1em">
+              <Button width="100%" disabled={disabled} align="center">
+                <FormattedMessage {...buttonsMessages.signIn} />
+              </Button>
+            </Box>
           </Box>
-        </Card>
-      </Form>
+        </Form>
+        <Link
+          marginTop="1em"
+          display="block"
+          antialiasing
+          to="/forgotPassword"
+          align="end"
+          size={-1}
+        >
+          <FormattedMessage {...authMessages.passwordForgotten} />
+        </Link>
+      </Box>
     );
   }
 }
