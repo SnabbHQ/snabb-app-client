36ae4323f02016615d21942e86a385fb4a4fa86f
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-03 15:45:46 +0100

    Fixed logout epic

--- src/common/auth/epics/logout.js
+++ src/common/auth/epics/logout.js
@@ -2,17 +2,15 @@
 import type { Deps } from '../../types';
 
 import { logOutSuccess } from '../actions';
-import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/observable/of';
-import 'rxjs/add/observable/fromPromise';
 import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 const logout = (action$: any, { authRepository }: Deps) =>
   action$.ofType('LOG_OUT')
-    .switchMap(() => Observable.fromPromise(authRepository.logout())
-      .map(logOutSuccess));
+    .switchMap(() => authRepository.logout())
+      .map(logOutSuccess);
 
 export default logout;
