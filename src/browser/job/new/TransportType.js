a93bb7fb93b3bbc8f4b699e944047bea9542ebad
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-04 17:45:33 +0100

    Fixed required uppercase issue

--- src/browser/job/new/TransportType.js
+++ src/browser/job/new/TransportType.js
@@ -7,11 +7,11 @@ import { Text, Image } from '../../app/components';
 const ICONS = {
   bike: require('../../../common/app/images/bike.svg'),
   car: require('../../../common/app/images/car.svg'),
-  cargobike: require('../../../common/app/images/cargobike.svg'),
+  cargoBike: require('../../../common/app/images/cargoBike.svg'),
   motorbike: require('../../../common/app/images/motorbike.svg'),
   van: require('../../../common/app/images/van.svg'),
   walk: require('../../../common/app/images/walk.svg'),
-  cargobikexl: require('../../../common/app/images/cargobikeXL.svg'),
+  cargoBikeXL: require('../../../common/app/images/cargoBikeXL.svg'),
 };
 
 const TransportType = ({ type, quotes }) => {
