57e36187f72363286ca12731d7a08e2da4cd2427
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-05 01:57:10 +0100

    Added ip whitelisting in server to restrict access

--- src/server/main.js
+++ src/server/main.js
@@ -3,8 +3,13 @@ import config from './config';
 import errorHandler from './lib/errorHandler';
 import express from 'express';
 import frontend from './frontend';
+import whitelist from './whitelist';
 
 const app = express();
+
+// As currently we are deploying to heroku and there is no way to IP restrict so far at the network
+// level, we need to configure express instead.
+app.use(whitelist);
 
 // $FlowFixMe
 app.use(frontend);
