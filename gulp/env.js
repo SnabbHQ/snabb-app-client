7bc4f8377e67fc426992635e245223e8886e527d
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-05 01:08:17 +0100

    Changed env git detection for Heroku

--- gulp/env.js
+++ gulp/env.js
@@ -6,7 +6,7 @@ gulp.task('env', () => {
   process.env.NODE_ENV = args.production ? 'production' : 'development';
   // The app is not a library, so it doesn't make sense to use semver.
   // Snabb uses appVersion for crash reporting to match bad builds easily.
-  const gitIsAvailable = !process.env.SOURCE_VERSION; // Heroku detection.
+  const gitIsAvailable = !process.env.HEROKU_RELEASE_VERSION; // Heroku detection.
   if (gitIsAvailable) {
     process.env.appVersion = execSync('git rev-parse HEAD').toString().trim();
   }
