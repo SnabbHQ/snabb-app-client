5f3099abc09f079d80844b99b79fd0bda0aad411
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-16 15:41:22 +0100

    Added action to appmessage

--- src/browser/app/AppMessage.js
+++ src/browser/app/AppMessage.js
@@ -1,31 +1,48 @@
 // @flow
+import R from 'ramda';
 import React from 'react';
+import { appShowMessage } from '../../common/app/actions';
 import authMessages from '../../common/auth/authMessages';
 import buttonsMessages from '../../common/app/buttonsMessages';
+import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import { Text, Box, Button, Space } from '../app/components';
 
-const AppMessage = () => (
-  <Box
-    display="flex"
-    backgroundColor="secondary"
-    paddingHorizontal={1}
-    paddingVertical={0.3}
-  >
-    <Text color="white">
-      <FormattedMessage {...authMessages.emailVerificationSent} />
-    </Text>
-    <Space auto />
-    <Button
-      white
-      size={-1}
-      bold
-      color="primary"
-      paddingHorizontal={2}
-    >
-      <FormattedMessage {...buttonsMessages.ok} />
-    </Button>
-  </Box>
-);
+type AppMessageProps = {
+  appShowMessage: any,
+};
 
-export default AppMessage;
+const AppMessage = ({ appShowMessage }: AppMessageProps) => {
+  const close = () => {
+    appShowMessage(false);
+  };
+
+  return (
+    <Box
+      display="flex"
+      backgroundColor="secondary"
+      paddingHorizontal={1}
+      paddingVertical={0.3}
+    >
+      <Text color="white">
+        <FormattedMessage {...authMessages.emailVerificationSent} />
+      </Text>
+      <Space auto />
+      <Button
+        white
+        size={-1}
+        bold
+        color="primary"
+        onClick={close}
+        paddingHorizontal={2}
+      >
+        <FormattedMessage {...buttonsMessages.ok} />
+      </Button>
+    </Box>
+  );
+};
+
+export default connect(
+  null,
+  { appShowMessage },
+)(AppMessage);
