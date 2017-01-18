c4f1fa9af896bce3c34513ebf116264110825cbf
Javier Tarazaga <javier.tarazaga@valtech.se>
2017-01-18 18:17:42 +0100

    Change approach to setup field values in inputs

--- src/browser/user/details/AccountInfoFields.js
+++ src/browser/user/details/AccountInfoFields.js
@@ -5,68 +5,90 @@ import React from 'react';
 import buttonMessages from '../../../common/app/buttonsMessages';
 import inputMessages from '../../../common/app/inputMessages';
 import linksMessages from '../../../common/app/linksMessages';
-import { updateProfile } from '../../../common/user/profile/actions';
+import {updateProfile} from '../../../common/user/profile/actions';
 import {connect} from 'react-redux';
-import { FormattedMessage, injectIntl } from 'react-intl';
-import { fields } from '../../../common/lib/redux-fields';
-import { FieldHeader, Form, Button, Space, Input, focus, Box } from '../../app/components';
+import {FormattedMessage, injectIntl} from 'react-intl';
+import {fields} from '../../../common/lib/redux-fields';
+import {FieldHeader, Form, Button, Space, Input, focus, Box} from '../../app/components';
 
 type AccountInfoFieldsProps = {
   profile: Profile
 }
 
-const AccountInfoFields = ({ disabled, error, fields, intl, profile, updateProfile }: AccountInfoFieldsProps) => {
 
-  const onFormSubmit = () => {
-    updateUserProfile();
+class AccountInfoFields extends React.Component {
+
+  constructor(props: P, context: any) {
+    super(props, context);
+
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.updateUserProfile = this.updateUserProfile.bind(this);
+  }
+
+  componentWillMount() {
+    console.log('hola')
+
+    const { profile, fields } = this.props;
+    fields['companyName'].value = profile.companyName;
+    fields['email'].value = profile.email;
+    fields['phone'].value = profile.phone;
+  }
+
+  onFormSubmit() {
+    this.updateUserProfile();
   };
 
-  const updateUserProfile = () => {
+  updateUserProfile() {
+    const { fields, profile, updateProfile } = this.props;
     updateProfile(profile.profileId, fields.$values());
   };
 
-  return (
-    <Box>
-      <FieldHeader titleSize={2} title={linksMessages.account} />
-      <Form onSubmit={onFormSubmit}>
-        <Input
-          error={error}
-          field={fields.companyName}
-          label={intl.formatMessage(inputMessages.name)}
-          maxLength={100}
-          placeholder={intl.formatMessage(inputMessages.name)}
-          defaultValue={profile.companyName}
-          type="text"
-        />
-        <Input
-          error={error}
-          field={fields.email}
-          label={intl.formatMessage(inputMessages.email)}
-          maxLength={100}
-          placeholder={intl.formatMessage(inputMessages.emailPlaceholder)}
-          defaultValue={profile.email}
-          type="text"
-        />
-        <Input
-          error={error}
-          field={fields.phone}
-          label={intl.formatMessage(inputMessages.phone)}
-          maxLength={100}
-          placeholder={intl.formatMessage(inputMessages.phonePlaceholder)}
-          defaultValue={profile.phone}
-          type="text"
-        />
-      </Form>
-      <Box display="flex" marginVertical={1} >
-        <Space auto />
-        <Space x={1} />
-        <Button accent paddingHorizontal={2} onClick={updateUserProfile} disabled={disabled}>
-          <FormattedMessage {...buttonMessages.save} />
-        </Button>
+  render() {
+
+    const {disabled, error, fields, intl} = this.props;
+
+    return (
+
+
+      <Box>
+        <FieldHeader titleSize={2} title={linksMessages.account} />
+        <Form onSubmit={this.onFormSubmit} >
+          <Input
+            error={error}
+            field={fields.companyName}
+            label={intl.formatMessage(inputMessages.name)}
+            maxLength={100}
+            placeholder={intl.formatMessage(inputMessages.name)}
+            type="text"
+          />
+          <Input
+            error={error}
+            field={fields.email}
+            label={intl.formatMessage(inputMessages.email)}
+            maxLength={100}
+            placeholder={intl.formatMessage(inputMessages.emailPlaceholder)}
+            type="text"
+          />
+          <Input
+            error={error}
+            field={fields.phone}
+            label={intl.formatMessage(inputMessages.phone)}
+            maxLength={100}
+            placeholder={intl.formatMessage(inputMessages.phonePlaceholder)}
+            type="text"
+          />
+        </Form>
+        <Box display="flex" marginVertical={1} >
+          <Space auto />
+          <Space x={1} />
+          <Button accent paddingHorizontal={2} onClick={this.updateUserProfile} disabled={disabled} >
+            <FormattedMessage {...buttonMessages.save} />
+          </Button>
+        </Box>
       </Box>
-    </Box>
-  );
-};
+    )
+  }
+}
 
 export default R.compose(
   connect(
@@ -75,11 +97,11 @@ export default R.compose(
       profile: state.user.profile,
       error: state.user.error,
     }),
-    { updateProfile },
+    {updateProfile},
   ),
   injectIntl,
   fields({
-    path: 'login',
+    path: 'account',
     fields: ['companyName', 'email', 'phone'],
   }),
   focus('error'),
